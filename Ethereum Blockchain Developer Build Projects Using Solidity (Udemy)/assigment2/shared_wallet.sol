47ced18a08e533896f79214966f510a26f2dd45b
Paulina Grunwald <paulina.grunwald@gmail.com>
2017-12-26 22:06:18 +0000

    update shared_wallet.sol

--- Ethereum Blockchain Developer Build Projects Using Solidity (Udemy)/assigment2/shared_wallet.sol
+++ Ethereum Blockchain Developer Build Projects Using Solidity (Udemy)/assigment2/shared_wallet.sol
@@ -1,17 +1,53 @@
-//contract version
+// Declare the source file compiler version.
 pragma solidity ^0.4.0;
+
+/* 'contract' has similarities to 'class' in other languages (class variables,
+inheritance, etc.) */
 
 contract MyContract {
     
     uint myVariable;
     
-    function MyContract() public{
+    address owner;
+
+    modifier onlyowner() {
+    	if(owner==msg.sender){
+    		_;
+    	} else {
+    		throw;
+
+    	}
+    }
+    //add contractor
+    // ether canbe sent
+    function MyContract() public payable { 
         myVariable = 5;
     }
     
-    function setMyVariable(uint myNewVariable) public {myVariable = myNewVariable;}   
+    //set variable function
+    function setMyVariable(uint myNewVariable) public {myVariable = myNewVariable;
+    }   
     
+    //add getter
+    //If the function is set as a constant function it will not consume
+    //gas during transaction
+
     function getMyVariable() public constant returns(uint){
-        return myVariable;
+    	if(msg.sender == owner) {
+    		myVariable = myNewVariable;
+    		}
+    }
+
+    //this represents address of this contract and banace represents
+    //balance of this contract
+    function getMyContractBalance() constant returns(uint){
+    	return this.balance;
+    	return myVariable;
+
+    }
+    //anonymus function (fallback function is used when we send data and 
+    //we don't have any function that match the data)
+    function() payable{
+
     }
 }
